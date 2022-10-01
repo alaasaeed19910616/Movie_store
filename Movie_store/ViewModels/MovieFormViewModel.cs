59f8ec9b9b89c4bcc76d74a9402c97d06ac06252
Alaa Saeed <alaa.saeed@niradynamics.se>
2022-10-01 13:46:12 +0200

    Add New Customer button to the main customer view
    Change-Id: I22aba949c5a1b525efa1c04044a774372a5a3e39

--- Movie_store/ViewModels/MovieFormViewModel.cs
+++ Movie_store/ViewModels/MovieFormViewModel.cs
@@ -9,7 +9,9 @@ namespace Movie_store.ViewModels
     public class MovieFormViewModel
     {
         public IEnumerable<Genre> Genres { get; set; }
+
         public Movie Movie { get; set; }
+
         public string Title
         {
             get
