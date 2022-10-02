07d37589870458c53672c0e22397d295a5ae9b14
Alaa Saeed <alaa.saeed@niradynamics.se>
2022-10-03 01:52:00 +0200

    Enable Facebook login
    Change-Id: I43b0ecd51c1a3e91c632d8b9d53bc87243a6475d

--- Movie_store/App_Start/FilterConfig.cs
+++ Movie_store/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace Movie_store
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
